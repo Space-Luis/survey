7063e16d07d9757b60fdadf1045e9c85240df69d
hingyi khong and maly phongsavanh <hingyi@>
2018-08-08 16:25:42 -0700

    add form, dropdown and radio input fields.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $("#nameInput").submit(function(event) {
+    event.preventDefault();
+    var name = $("#nameInput").val();
+    var ice-cream = $("#ice-cream").val();
